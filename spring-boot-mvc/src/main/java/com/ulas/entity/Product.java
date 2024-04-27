ce8a71e7c881e35d8f2fa0b31a1bdd30874b41e1
Ulasgltkn <ulass.gultekin@gmail.com>
2024-04-27 19:20:57 +0300

    add project

---
+++ spring-boot-mvc/src/main/java/com/ulas/entity/Product.java
@@ -0,0 +1,25 @@
+package com.ulas.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String imageFileName;
+    private Date createdAt;
+}
